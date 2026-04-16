89606c18ded4aade748e03cc2971bebe61c89c16
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2026-04-16 19:29:04 -0400

    Update eff_video_shader.js

--- src/effects/eff_video_shader.js
+++ src/effects/eff_video_shader.js
@@ -1,36 +1,27 @@
 //
 export default class eff_video_shader {
-  static meta_props = [
-    { prop: 'expand', selection: [1, 2, 3] },
-    { prop: 'step', selection: [1, 0.1, 0.2, 2, 4] },
-    { prop: 'period', selection: [-1, 0, 1, 2, 3] },
-  ];
-
   constructor(props) {
-    //
     Object.assign(this, props);
-    console.log('eff_video_shader props.expand', props.expand);
-    // console.log('eff_a_slit_scan constructor width, height', width, height);
-
-    this.vw = this.input.width;
-    this.vh = this.input.height;
-    console.log('eff_video_shader constructor input vw vh', this.vw, this.vh);
-
-    // !!@ needed after reset
-    let expand = this.expand || 1;
-    this.output = createGraphics(this.vw * expand, this.vh);
-    // this.output.background(255);
-
-    this.x = 0;
+    // console.log('eff_shader_clamp props', props);
+    this.init();
   }
   prepareOutput() {
-    // console.log('eff_a_slit_scan prepareOutput output', this.output);
-    this.input.loadPixels();
-    this.output.copy(this.input, this.vw / 2, 0, 1, this.vh, this.x, 0, 1, this.vh);
-    this.x = this.x + this.step;
-    if (this.x > this.output.width) {
-      this.x = 0;
-    }
+    // console.log('eff_shader_clamp prepareOutput');
+    this.output.shader(this.aShader);
+
+    // passing cam as a texture
+    this.aShader.setUniform('tex0', this.input);
+
+    // rect gives us some geometry on the screen
+    this.output.rect(0, 0, this.input.width, this.input.height);
+  }
+  init() {
+    let { width, height } = this.input;
+
+    this.aShader = createShader(a_vert, a_frag);
+
+    this.output = createGraphics(width, height, WEBGL);
+    this.output.clear();
   }
 }
 
